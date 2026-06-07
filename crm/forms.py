3907480f3655c27e16e9612f5b52cc58f23f3f7d
CalmCompiler06 <durejavedika1@gmail.com>
2026-06-07 23:51:40 +0530

    Improved UI for Products Regions and Leads

--- crm/forms.py
+++ crm/forms.py
@@ -31,9 +31,14 @@ class LeadForm(forms.ModelForm):
     class Meta:
         model = Lead
 
-        exclude = ['leadid','added_by', 'added_dts']
+        exclude = [
+            'leadid',
+            'added_by',
+            'added_dts'
+        ]
 
         widgets = {
+
             'lead_gen_date': forms.DateInput(
                 attrs={
                     'type': 'date',
@@ -43,7 +48,20 @@ class LeadForm(forms.ModelForm):
 
             'businessneed': forms.Textarea(
                 attrs={
-                    'rows': 3
+                    'rows': 3,
+                    'class': 'form-control'
                 }
             )
         }
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+
+            if field_name not in ['lead_gen_date', 'businessneed']:
+
+                field.widget.attrs.update({
+                    'class': 'form-control'
+                })
